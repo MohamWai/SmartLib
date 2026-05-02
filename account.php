12e85288a4395547d15c96297a37f963e2e64617
Nasser Alriyami <s145224@student.squ.edu.om>
2026-05-02 17:20:09 +0400

    Update account.php

--- account.php
+++ account.php
@@ -184,6 +184,7 @@ if ($bq && $bq->num_rows > 0) {
         </p>
       </div>
     </footer>
+      
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
   </body>
